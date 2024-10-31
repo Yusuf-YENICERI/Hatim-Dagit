232d632cbc7c681e14d9db063e166459576abdb6
Yusuf-YENICERI <joshard20917@gmail.com>
2024-11-01 00:49:54 +0300

    BİSMİLLAHİRRAHMANİRRAHİM some fix

--- src/components/common/CookieConsent/index.tsx
+++ src/components/common/CookieConsent/index.tsx
@@ -23,16 +23,16 @@ export function CookiesBanner() {
 
   return (
     <Paper withBorder p="lg" radius="md" shadow="md" style={{display: cookieVisibility ? 'inline-block' : 'none', position: 'fixed', zIndex: 999, bottom: 80, left: 0, right: 0}}>
-      <Group justify="space-between" mb="xs">
-        <Text fz="md" fw={500}>
+      <Group  mb="xs">
+        <Text  >
           Çerez Bildirimi
         </Text>
         <CloseButton mr={-9} mt={-9} onClick={() => setCookieVisibility(false)} />
       </Group>
-      <Text c="dimmed" fz="xs">
+      <Text  >
       Bu internet sitesinde, kullanıcı deneyimini geliştirmek ve internet sitesinin verimli çalışmasını sağlamak amacıyla çerezler kullanılmaktadır. Bu internet sitesini kullanarak bu çerezlerin kullanılmasını kabul etmiş olursunuz. Çerezleri nasıl kullandığımız, sildiğimiz ve engellediğimiz ile ilgili detaylı bilgi için lütfen Çerez Politikası sayfasını okuyunuz: <a href='https://hatimdagit.com/cookies'>Çerez Politikası</a>
       </Text>
-      <Group justify="flex-end" mt="md">
+      <Group mt="md">
         <Button variant="outline" size="xs" onClick={() => {
             localDb.ref('cookieStuff').set({
                 cookieBannerAccepted: true});
